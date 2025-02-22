f0d85cf3a87f92abf1fff9a07efa31a7483d1b03
Volodymyr S. <26582191+SweetOps@users.noreply.github.com>
2025-02-22 12:47:58 -0500

    feat: initial implementation (#1)

---
+++ cql.go
@@ -0,0 +1,158 @@
+package cql
+
+import (
+	"errors"
+	"time"
+
+	"github.com/gocql/gocql"
+	"go.k6.io/k6/js/modules"
+)
+
+const ImportPath = "k6/x/cql"
+
+//nolint:gochecknoinits // This is the recommended way to register the module.
+func init() {
+	modules.Register(ImportPath, new(RootModule))
+}
+
+// RootModule is the global module object type. It is instantiated once per test
+// run and will be used to create `k6/x/exec` module instances for each VU.
+type RootModule struct{}
+
+type CQL struct {
+	vu      modules.VU
+	session *gocql.Session
+}
+
+type Config struct {
+	Timeout  string    `json:"timeout"`
+	Hosts    []string  `json:"hosts"`
+	Username string    `json:"username"`
+	Password string    `json:"password"`
+	Keyspace string    `json:"keyspace"`
+	TLS      ConfigTLS `json:"tls"`
+}
+
+type ConfigTLS struct {
+	CertPath               string `json:"cert_path"`
+	KeyPath                string `json:"key_path"`
+	CaPath                 string `json:"ca_path"`
+	EnableHostVerification bool   `json:"enable_host_verification"`
+}
+
+// Ensure the interfaces are implemented correctly.
+var (
+	_                 modules.Module   = &RootModule{}
+	_                 modules.Instance = &CQL{}
+	ConnectionTimeout                  = 10 * time.Second
+)
+
+// NewModuleInstance implements the modules.Module interface to return
+// a new instance for each VU.
+func (*RootModule) NewModuleInstance(vu modules.VU) modules.Instance {
+	return &CQL{vu: vu}
+}
+
+// Exports implements the modules.Instance interface and returns the exports
+// of the JS module.
+func (cql *CQL) Exports() modules.Exports {
+	return modules.Exports{Default: cql}
+}
+
+func (cql *CQL) Session(cfg Config) error {
+	if len(cfg.Hosts) == 0 || cfg.Keyspace == "" {
+		return errors.New("hosts and keyspace are required parameters")
+	}
+
+	cluster := gocql.NewCluster(cfg.Hosts...)
+	cluster.Keyspace = cfg.Keyspace
+
+	if cfg.Timeout == "" {
+		cluster.Timeout = ConnectionTimeout
+	} else {
+		timeout, err := time.ParseDuration(cfg.Timeout)
+		if err != nil {
+			return errors.New("invalid timeout value: " + err.Error())
+		}
+		cluster.Timeout = timeout
+	}
+
+	if cfg.Username != "" && cfg.Password != "" {
+		cluster.Authenticator = gocql.PasswordAuthenticator{
+			Username: cfg.Username,
+			Password: cfg.Password,
+		}
+	}
+
+	if cfg.TLS.CertPath != "" && cfg.TLS.KeyPath != "" {
+		cluster.SslOpts = &gocql.SslOptions{
+			CertPath: cfg.TLS.CertPath,
+			KeyPath:  cfg.TLS.KeyPath,
+		}
+	}
+
+	if cfg.TLS.CaPath != "" {
+		cluster.SslOpts = &gocql.SslOptions{
+			CaPath: cfg.TLS.CaPath,
+		}
+	}
+
+	if cfg.TLS.EnableHostVerification {
+		cluster.SslOpts = &gocql.SslOptions{
+			EnableHostVerification: cfg.TLS.EnableHostVerification,
+		}
+	}
+
+	session, err := cluster.CreateSession()
+	if err != nil {
+		return err
+	}
+	cql.session = session
+	return nil
+}
+
+func (cql *CQL) Exec(query string) error {
+	if err := cql.isConnected(); err != nil {
+		return err
+	}
+
+	return cql.session.Query(query).Exec()
+}
+
+func (cql *CQL) Close() {
+	if cql.session != nil {
+		cql.session.Close()
+	}
+}
+
+func (cql *CQL) Batch(batchType string, queries []string) error {
+	if err := cql.isConnected(); err != nil {
+		return err
+	}
+
+	bType := cql.resolveBatchType(batchType)
+	b := cql.session.NewBatch(bType)
+	for _, query := range queries {
+		b.Query(query)
+	}
+
+	return cql.session.ExecuteBatch(b)
+}
+
+func (cql *CQL) resolveBatchType(batchType string) gocql.BatchType {
+	switch batchType {
+	case "unlogged":
+		return gocql.UnloggedBatch
+	case "counter":
+		return gocql.CounterBatch
+	default:
+		return gocql.LoggedBatch
+	}
+}
+
+func (cql *CQL) isConnected() error {
+	if cql.session == nil {
+		return errors.New("not connected to a cluster")
+	}
+	return nil
+}
---
+++ cql_test.go
@@ -0,0 +1,50 @@
+package cql_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	xk6_cql "github.com/SweetOps/xk6-cql"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go/modules/cassandra"
+)
+
+var (
+	CassandraImage = "cassandra:4.0.17"
+)
+
+func runCassandra(ctx context.Context, image string) (*cassandra.CassandraContainer, error) {
+	return cassandra.Run(
+		ctx,
+		image,
+	)
+}
+
+func Test_CQL(t *testing.T) {
+	ctx := context.Background()
+	c, err := runCassandra(ctx, CassandraImage)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer c.Terminate(ctx) //nolint:errcheck // Error is not important here
+
+	host, err := c.ConnectionHost(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	time.Sleep(30 * time.Second)
+
+	cql := xk6_cql.CQL{}
+
+	err = cql.Session(xk6_cql.Config{
+		Hosts:    []string{host},
+		Keyspace: "system",
+	})
+	require.NoError(t, err)
+
+	err = cql.Exec("SELECT * FROM local")
+	assert.NoError(t, err)
+}
