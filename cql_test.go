81d9c85c87b2b9af337f9cc13b9d12ca8b73075b
Volodymyr Syromiatnykov <vladimir.fess@gmail.com>
2025-02-22 12:14:45 -0500

    feat: initial implementation

---
+++ cql_test.go
@@ -0,0 +1,48 @@
+package cql_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	xk6_cql "github.com/SweetOps/xk6-cql"
+	"github.com/stretchr/testify/assert"
+	"github.com/testcontainers/testcontainers-go/modules/cassandra"
+)
+
+var (
+	CassandraImage = "cassandra:4.0.17"
+)
+
+func runCassandra(ctx context.Context, image string) (*cassandra.CassandraContainer, error) {
+	return cassandra.Run(
+		ctx,
+		image,
+	)
+}
+
+func Test_CQL(t *testing.T) {
+	ctx := context.Background()
+	c, err := runCassandra(ctx, CassandraImage)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer c.Terminate(ctx)
+
+	host, err := c.ConnectionHost(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	time.Sleep(30 * time.Second)
+
+	cql := xk6_cql.CQL{}
+
+	err = cql.Session(xk6_cql.Config{
+		Hosts:    []string{host},
+		Keyspace: "system",
+	})
+	assert.NoError(t, err)
+
+	err = cql.Exec("SELECT * FROM local")
+	assert.NoError(t, err)
+}
---
+++ examples/run.js
@@ -0,0 +1,80 @@
+import cql from 'k6/x/cql';
+
+export let options = {
+    scenarios: {
+        setup: {
+            executor: 'shared-iterations',
+            vus: 1,
+            iterations: 1,
+            exec: 'setup',
+            startTime: '0s',
+        },
+        stress_test: {
+            executor: 'ramping-vus',
+            stages: [
+                { duration: '10s', target: 3 },
+                { duration: '5s', target: 1 },
+                { duration: '1s', target: 0 }
+            ],
+            exec: 'stressTest',
+            startTime: '10s'
+        },
+    }
+};
+
+export function setup() {
+    let client = cql;
+    let hosts = __ENV.CQL_HOSTS || "127.0.0.1:9042";
+    const keyspace = "test_keyspace";
+
+    try {
+        client.session({
+            hosts: hosts.split(","),
+            keyspace: "system",
+        });
+
+        console.log("Creating keyspace...");
+        client.exec(`
+            CREATE KEYSPACE IF NOT EXISTS test_keyspace 
+            WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};
+        `);
+    } catch (error) {
+        console.error("Failed to create keyspace", error);
+        throw error;
+    }
+
+    try {
+        client.session({
+            hosts: hosts.split(","),
+            keyspace: keyspace,
+        });
+
+        console.log("Creating table...");
+        client.exec(`
+            CREATE TABLE IF NOT EXISTS users (
+                id UUID PRIMARY KEY, 
+                name TEXT
+            );
+        `);
+    } catch (error) {
+        console.error("Failed to create table", error);
+        throw error;
+    }
+}
+
+export function stressTest() {
+    let client = cql;
+    let hosts = __ENV.CQL_HOSTS || "127.0.0.1:9042";
+    const keyspace = "test_keyspace";
+
+    client.session({
+        hosts: hosts.split(","),
+        keyspace: keyspace,
+    });
+
+    let id = Math.floor(Math.random() * 1000000);
+    let name = `User${id}`;
+
+    client.exec(`INSERT INTO users (id, name) VALUES (uuid(), '${name}');`);
+    client.close();
+}
